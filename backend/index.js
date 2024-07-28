a2065036f7a17834eaf7fd5c54c6cacb78ea07b7
“Tarun <59139915+Tarun-Vaghela90@users.noreply.github.com>
2024-07-28 16:01:55 +0530

    added group_jay  projects

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,16 @@ app.use(express.json());
 //     console.error('Error connecting to MongoDB:', err.message);
 // });
 
+// const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/your_database_name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World! This is the start of a new world');
